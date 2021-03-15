1465ba98d4112dd13190226826dc945237bd573b
kunansy <kolobov.kirill@list.ru>
2021-03-15 22:02:09 +0300

    sanic application started

--- src/main.py
+++ src/main.py
@@ -1,5 +1,30 @@
-#!/usr/bin/env pthon3
+#!/usr/bin/env python3
+
+import argparse
+
+from sanic import Sanic
+from sanic.request import Request
+from sanic.response import json
+
+app = Sanic(__name__)
+
+
+@app.route("/")
+async def home(request: Request):
+    return json({"Method": request.method})
+
 
 if __name__ == "__main__":
-    pass
+    parser = argparse.ArgumentParser(
+        description=""
+    )
+    parser.add_argument(
+        '--debug',
+        help="If true run sanic in debug mode",
+        action="store_true",
+        default=False,
+        dest="debug"
+    )
+    args = parser.parse_args()
 
+    app.run(host="0.0.0.0", port=8080, debug=args.debug)
