9a67fe746520a12d4ca28a6d80250afed31bff8d
kunansy <kolobov.kirill@list.ru>
2021-03-18 21:34:47 +0300

    tests for Order model moved to special file

--- tests/model_test.py
+++ tests/model_test.py
@@ -3,37 +3,5 @@ import pytest
 from src.model import CourierModel, OrderModel, TimeSpan
 
 
-def test_order_with_right_fields():
-    pass
-
-
-def test_order_with_zero_id():
-    pass
-
-
-def test_order_with_zero_weight():
-    pass
-
-
-def test_order_with_big_weight():
-    pass
-
-
-def test_order_with_zero_region():
-    pass
-
-
-def test_order_with_empty_region_list():
-    pass
-
-
-def test_order_with_not_serializable_delivery_hours():
-    pass
-
-
-def test_order_time_format():
-    pass
-
-
 if __name__ == "__main__":
     pytest.main(['-svv'])
