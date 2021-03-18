383af553751a3a7f2d032edb4f9742306008980b
kunansy <kolobov.kirill@list.ru>
2021-03-18 21:32:57 +0300

    tests for Courier model moved to special file

---
+++ tests/courier_test.py
@@ -0,0 +1,39 @@
+import pytest
+
+from src.model import CourierModel, TimeSpan
+
+
+def test_with_right_fields():
+    pass
+
+
+def test_with_zero_id():
+    pass
+
+
+def test_with_wrong_type():
+    pass
+
+
+def test_with_empty_type():
+    pass
+
+
+def test_with_zero_region():
+    pass
+
+
+def test_with_empty_region_list():
+    pass
+
+
+def test_with_not_serializable_working_hours():
+    pass
+
+
+def test_time_format():
+    pass
+
+
+if __name__ == "__main__":
+    pytest.main(['-svv'])
