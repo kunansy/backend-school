141ba6aa933f73de38025760d0805b71331dd20a
kunansy <kolobov.kirill@list.ru>
2021-03-18 21:14:29 +0300

    tests for Courier model started

--- tests/model_test.py
+++ tests/model_test.py
@@ -1,6 +1,39 @@
 import pytest
 
+from src.model import CourierModel, OrderModel, TimeSpan
+
+
+def test_courier_with_right_fields():
+    pass
+
+
+def test_courier_with_zero_id():
+    pass
+
+
+def test_courier_with_wrong_courier_type():
+    pass
+
+
+def test_courier_with_empty_courier_type():
+    pass
+
+
+def test_courier_with_zero_region():
+    pass
+
+
+def test_courier_with_empty_region_list():
+    pass
+
+
+def test_courier_with_not_serializable_working_hours():
+    pass
+
+
+def test_courier_time_format():
+    pass
+
 
 if __name__ == "__main__":
     pytest.main(['-svv'])
-
