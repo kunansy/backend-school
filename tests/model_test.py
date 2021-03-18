40dd2512b97250eb2755214055036d5db401c10a
kunansy <kolobov.kirill@list.ru>
2021-03-18 21:28:20 +0300

    tests for Order model started

--- tests/model_test.py
+++ tests/model_test.py
@@ -35,5 +35,37 @@ def test_courier_time_format():
     pass
 
 
+def test_order_with_right_fields():
+    pass
+
+
+def test_order_with_zero_id():
+    pass
+
+
+def test_order_with_zero_weight():
+    pass
+
+
+def test_order_with_big_weight():
+    pass
+
+
+def test_order_with_zero_region():
+    pass
+
+
+def test_order_with_empty_region_list():
+    pass
+
+
+def test_order_with_not_serializable_delivery_hours():
+    pass
+
+
+def test_order_time_format():
+    pass
+
+
 if __name__ == "__main__":
     pytest.main(['-svv'])
