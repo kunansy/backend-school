6dc740961a35e5d2a930c3a952c80247a4c50c0e
kunansy <kolobov.kirill@list.ru>
2021-03-19 18:54:27 +0300

    some SpanTime tests added

--- tests/time_span_test.py
+++ tests/time_span_test.py
@@ -4,18 +4,35 @@ from src.model import TimeSpan
 
 
 def test_without_dash():
-    pass
+    with pytest.raises(ValueError):
+        TimeSpan('12:35_12:36')
 
 
 def test_wrong_time_format():
-    pass
+    with pytest.raises(ValueError):
+        TimeSpan('12.00-13.00')
+
+
+def test_wrong_hours():
+    with pytest.raises(ValueError):
+        TimeSpan('27:00-12:00')
+
+
+def test_wrong_minutes():
+    with pytest.raises(ValueError):
+        TimeSpan('12:56-13:66')
 
 
 def test_start_less_than_stop():
+    with pytest.raises(ValueError):
+        TimeSpan('12:00-06:59')
+
+
+def test_intercept_equal_to_or_with_true():
     pass
 
 
-def test_intercept_equal_to_or():
+def test_intercept_equal_to_or_with_false():
     pass
 
 
@@ -40,15 +57,21 @@ def test_spans_with_the_same_stop():
 
 
 def test_repr():
-    pass
+    res = "14:44-21:12"
+
+    assert str(TimeSpan(res)) == res
 
 
 def test_start_property():
-    pass
+    t = TimeSpan('12:50-23:19')
+
+    assert t.start == t._TimeSpan__start
 
 
 def test_stop_property():
-    pass
+    t = TimeSpan('12:50-23:19')
+
+    assert t.stop == t._TimeSpan__stop
 
 
 if __name__ == "__main__":
