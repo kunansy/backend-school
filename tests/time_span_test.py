882523d1676f6613cfb6fd09efd51cd74042db05
kunansy <kolobov.kirill@list.ru>
2021-03-19 10:48:35 +0300

    time span tests started

--- tests/time_span_test.py
+++ tests/time_span_test.py
@@ -3,5 +3,53 @@ import pytest
 from src.model import TimeSpan
 
 
+def test_without_dash():
+    pass
+
+
+def test_wrong_time_format():
+    pass
+
+
+def test_start_less_than_stop():
+    pass
+
+
+def test_intercept_equal_to_or():
+    pass
+
+
+def test_not_intercepting_spans():
+    pass
+
+
+def test_equal_spans():
+    pass
+
+
+def test_intercepting_spans():
+    pass
+
+
+def test_spans_with_the_same_start():
+    pass
+
+
+def test_spans_with_the_same_stop():
+    pass
+
+
+def test_repr():
+    pass
+
+
+def test_start_property():
+    pass
+
+
+def test_stop_property():
+    pass
+
+
 if __name__ == "__main__":
     pytest.main(['-svv'])
