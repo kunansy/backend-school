afac26c9300184f089b4ffe83c961434fbfdc999
kunansy <kolobov.kirill@list.ru>
2021-03-19 11:32:58 +0300

    get_courier() and update_courier() started

--- src/db_api.py
+++ src/db_api.py
@@ -8,3 +8,14 @@ def get_courier_types() -> list[str]:
 async def add_couriers(couriers: list[CourierModel]) -> None:
     # logger.info(f"{len(couriers)} added to database")
     pass
+
+
+async def get_courier(value,
+                      field: str = 'id') -> CourierModel:
+    pass
+
+
+async def update_courier(courier: CourierModel) -> None:
+    # await assign_order(courier.courier_id)
+    # logger.info(f"{courier.courier_id} updated")
+    pass
