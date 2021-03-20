d1c9d939a13b4f3291e7fa4efea8b3d96f354d3c
kunansy <kolobov.kirill@list.ru>
2021-03-20 21:44:08 +0300

    some funcs in db_api started

--- src/db_api.py
+++ src/db_api.py
@@ -16,3 +16,24 @@ async def update_courier(courier) -> None:
     # await assign_order(courier.courier_id)
     # logger.info(f"{courier.courier_id} updated")
     pass
+
+
+async def get_order(value,
+                    field: str = 'order_id'):
+    pass
+
+
+async def add_orders(order: list) -> None:
+    pass
+
+
+async def assign_orders(courier_id: int) -> list:
+    pass
+
+
+async def assign_info(order_id: int):
+    pass
+
+
+async def complete_order(complete) -> None:
+    pass
