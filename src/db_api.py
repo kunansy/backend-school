8dbad97af2d9561358a4d288794ed0a0c094b0a0
kunansy <kolobov.kirill@list.ru>
2021-03-20 21:43:46 +0300

    type hints changed in db_api
    otherwise there's cycle import error

--- src/db_api.py
+++ src/db_api.py
@@ -1,21 +1,18 @@
-from src.model import CourierModel
-
-
 def get_courier_types() -> list[str]:
     return ['foot', 'car', 'bike']
 
 
-async def add_couriers(couriers: list[CourierModel]) -> None:
+async def add_couriers(couriers: list) -> None:
     # logger.info(f"{len(couriers)} added to database")
     pass
 
 
 async def get_courier(value,
-                      field: str = 'id') -> CourierModel:
+                      field: str = 'courier_id'):
     pass
 
 
-async def update_courier(courier: CourierModel) -> None:
+async def update_courier(courier) -> None:
     # await assign_order(courier.courier_id)
     # logger.info(f"{courier.courier_id} updated")
     pass
