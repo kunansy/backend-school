03e29c148f098daf8840a875a331bdd2bafcbb92
kunansy <kolobov.kirill@list.ru>
2021-03-21 17:44:42 +0300

    app tests started

--- tests/app_test.py
+++ tests/app_test.py
@@ -1,7 +1,82 @@
 #!/usr/bin/env python3
-
+import mock
 import pytest
-from src.server import app
+import src.server as server
+
+
+@mock.patch("src.server.db_api.add_couriers")
+def test_add_couriers_with_valid_couriers(db_mock: mock.AsyncMock):
+    pass
+
+
+@mock.patch("src.server.db_api.add_couriers")
+def test_add_couriers_with_invalid_couriers(db_mock: mock.AsyncMock):
+    pass
+
+
+@mock.patch("src.server.db_api.get_courier")
+def test_update_courier_with_no_courier(get_courier_mock: mock.AsyncMock):
+    pass
+
+
+@mock.patch("src.server.db_api.get_courier")
+def test_update_courier_with_unpatchable_fields(get_courier_mock: mock.AsyncMock):
+    pass
+
+
+@mock.patch("src.server.db_api.get_courier")
+def test_update_courier_with_invalid_fields(get_courier_mock: mock.AsyncMock):
+    pass
+
+
+@mock.patch("src.server.db_api.get_courier")
+def test_update_courier_with_invalid_fields(get_courier_mock: mock.AsyncMock):
+    pass
+
+
+@mock.patch("src.server.db_api.get_courier")
+@mock.patch("src.server.db_api.update_courier")
+def test_update_courier_with_ok(get_courier_mock: mock.AsyncMock,
+                                update_courier_mock: mock.AsyncMock):
+    pass
+
+
+@mock.patch("src.server.db_api.get_courier")
+def test_get_courier(get_courier_mock: mock.AsyncMock):
+    pass
+
+
+@mock.patch("src.server.db_api.add_orders")
+def test_add_orders_with_valid_orders(add_orders_mock: mock.AsyncMock):
+    pass
+
+
+@mock.patch("src.server.db_api.add_orders")
+def test_add_orders_with_invalid_orders(add_orders_mock: mock.AsyncMock):
+    pass
+
+
+@mock.patch("src.server.db_api.get_courier")
+def test_assign_with_no_courier(get_courier_mock: mock.AsyncMock):
+    pass
+
+
+@mock.patch("src.server.db_api.get_courier")
+@mock.patch("src.server.db_api.assign_orders")
+def test_assign_with_no_orders(get_courier_mock: mock.AsyncMock,
+                               assign_orders_mock: mock.AsyncMock):
+    pass
+
+
+@mock.patch("src.server.db_api.get_courier")
+@mock.patch("src.server.db_api.assign_orders")
+def test_assign_with_ok(get_courier_mock: mock.AsyncMock,
+                        assign_orders_mock: mock.AsyncMock):
+    pass
+
+
+def test_complete_with_invalid_request():
+    pass
 
 
 if __name__ == '__main__':
