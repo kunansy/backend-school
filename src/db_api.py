26ececbb7745d6e05a279499abf311516315df99
kunansy <kolobov.kirill@list.ru>
2021-03-23 10:08:32 +0300

    use class in db_api

--- src/db_api.py
+++ src/db_api.py
@@ -1,42 +1,47 @@
 from typing import List
 
 
-def get_courier_types() -> List[str]:
-    return ['foot', 'car', 'bike']
 
+class Database:
+    def __init__(self) -> None:
+        pass
 
-async def add_couriers(couriers: list) -> None:
-    # logger.info(f"{len(couriers)} added to database")
-    pass
+    async def get_courier_types(self) -> List[str]:
+        return ['foot', 'car', 'bike']
 
+    async def add_couriers(self,
+                           couriers: list) -> None:
+        # logger.info(f"{len(couriers)} added to database")
+        pass
 
-async def get_courier(value,
-                      field: str = 'courier_id'):
-    pass
+    async def get_courier(self,
+                          value,
+                          field: str = 'courier_id'):
+        pass
 
+    async def update_courier(self,
+                             courier) -> None:
+        # await assign_order(courier.courier_id)
+        # logger.info(f"{courier.courier_id} updated")
+        pass
 
-async def update_courier(courier) -> None:
-    # await assign_order(courier.courier_id)
-    # logger.info(f"{courier.courier_id} updated")
-    pass
+    async def get_order(self,
+                        value,
+                        field: str = 'order_id'):
+        pass
 
+    async def add_orders(self,
+                         order: list) -> None:
+        pass
 
-async def get_order(value,
-                    field: str = 'order_id'):
-    pass
+    async def assign_orders(self,
+                            courier_id: int) -> list:
+        pass
 
+    async def assign_info(self,
+                          order_id: int):
+        pass
 
-async def add_orders(order: list) -> None:
-    pass
-
-
-async def assign_orders(courier_id: int) -> list:
-    pass
-
-
-async def assign_info(order_id: int):
-    pass
-
-
-async def complete_order(complete) -> None:
-    pass
+    async def complete_order(self,
+                             complete) -> None:
+        pass
