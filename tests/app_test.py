ac988033a8265ca7c33332127bd01aa42c0952c6
kunansy <kolobov.kirill@list.ru>
2021-03-23 15:19:37 +0300

    mock denotation fixed

--- tests/app_test.py
+++ tests/app_test.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python3
+import logging
+
 import mock
 import pytest
+
 import src.server as server
+from src.model import CourierModel
+from src.server import app
+
+logging.disable(logging.CRITICAL)
 
 
 @mock.patch("src.server.db_api.add_couriers")
@@ -9,67 +16,68 @@ def test_add_couriers_with_valid_couriers(db_mock: mock.AsyncMock):
     pass
 
 
-@mock.patch("src.server.db_api.add_couriers")
+
+@mock.patch("src.server.app.db.add_couriers")
 def test_add_couriers_with_invalid_couriers(db_mock: mock.AsyncMock):
     pass
 
 
-@mock.patch("src.server.db_api.get_courier")
+@mock.patch("src.server.app.db.get_courier")
 def test_update_courier_with_no_courier(get_courier_mock: mock.AsyncMock):
     pass
 
 
-@mock.patch("src.server.db_api.get_courier")
+@mock.patch("src.server.app.db.get_courier")
 def test_update_courier_with_unpatchable_fields(get_courier_mock: mock.AsyncMock):
     pass
 
 
-@mock.patch("src.server.db_api.get_courier")
+@mock.patch("src.server.app.db.get_courier")
 def test_update_courier_with_invalid_fields(get_courier_mock: mock.AsyncMock):
     pass
 
 
-@mock.patch("src.server.db_api.get_courier")
+@mock.patch("src.server.app.db.get_courier")
 def test_update_courier_with_invalid_fields(get_courier_mock: mock.AsyncMock):
     pass
 
 
-@mock.patch("src.server.db_api.get_courier")
-@mock.patch("src.server.db_api.update_courier")
+@mock.patch("src.server.app.db.get_courier")
+@mock.patch("src.server.app.db.update_courier")
 def test_update_courier_with_ok(get_courier_mock: mock.AsyncMock,
                                 update_courier_mock: mock.AsyncMock):
     pass
 
 
-@mock.patch("src.server.db_api.get_courier")
+@mock.patch("src.server.app.db.get_courier")
 def test_get_courier(get_courier_mock: mock.AsyncMock):
     pass
 
 
-@mock.patch("src.server.db_api.add_orders")
+@mock.patch("src.server.app.db.add_orders")
 def test_add_orders_with_valid_orders(add_orders_mock: mock.AsyncMock):
     pass
 
 
-@mock.patch("src.server.db_api.add_orders")
+@mock.patch("src.server.app.db.add_orders")
 def test_add_orders_with_invalid_orders(add_orders_mock: mock.AsyncMock):
     pass
 
 
-@mock.patch("src.server.db_api.get_courier")
+@mock.patch("src.server.app.db.get_courier")
 def test_assign_with_no_courier(get_courier_mock: mock.AsyncMock):
     pass
 
 
-@mock.patch("src.server.db_api.get_courier")
-@mock.patch("src.server.db_api.assign_orders")
+@mock.patch("src.server.app.db.get_courier")
+@mock.patch("src.server.app.db.assign_orders")
 def test_assign_with_no_orders(get_courier_mock: mock.AsyncMock,
                                assign_orders_mock: mock.AsyncMock):
     pass
 
 
-@mock.patch("src.server.db_api.get_courier")
-@mock.patch("src.server.db_api.assign_orders")
+@mock.patch("src.server.app.db.get_courier")
+@mock.patch("src.server.app.db.assign_orders")
 def test_assign_with_ok(get_courier_mock: mock.AsyncMock,
                         assign_orders_mock: mock.AsyncMock):
     pass
