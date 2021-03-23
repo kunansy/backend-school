18a59da4d87f725cd4ac7e16c760622dd49d6f7e
kunansy <kolobov.kirill@list.ru>
2021-03-23 14:17:58 +0300

    use parametrize tests in time_span_test

--- tests/time_span_test.py
+++ tests/time_span_test.py
@@ -6,94 +6,48 @@ import pytest
 from src.model import TimeSpan
 
 
-def test_without_dash():
+@pytest.mark.parametrize(
+    'value', (
+        '12:35_12:36', '12.00-13.00',
+        '27:00-12:00', '12:56-13:66',
+        '12:00-06:59', ''
+    )
+)
+def test_wrong_format(value):
     with pytest.raises(ValueError):
-        TimeSpan('12:35_12:36')
+        TimeSpan(value)
 
 
-def test_wrong_time_format():
-    with pytest.raises(ValueError):
-        TimeSpan('12.00-13.00')
+@pytest.mark.parametrize(
+    ('t1', 't2', 'result'), (
+        ('12:10-13:50', '13:40-14:10', True),
+        ('12:10-12:20', '12:10-12:20', True),
+        ('21:55-23:15', '22:00-23:45', True),
+        ('10:55-23:15', '12:00-19:45', True),
+        ('12:10-12:20', '12:21-12:50', False),
+        ('12:10-12:20', '12:21-12:50', False),
+        ('12:10-12:20', '06:21-10:50', False),
+    )
+)
+def test_intercepting(t1, t2, result):
+    t1, t2 = TimeSpan(t1), TimeSpan(t2)
+
+    assert t1.is_intercept(t2) is t1 | t2 is result
+    assert t2.is_intercept(t1) is t2 | t1 is result
 
 
-def test_wrong_hours():
-    with pytest.raises(ValueError):
-        TimeSpan('27:00-12:00')
+@pytest.mark.parametrize(
+    ('t1', 't2', 'result', 'time_'), (
+        ('21:55-22:15', '21:55-23:45', True, time(21, 55)),
+        ('10:31-20:50', '17:55-20:50', True, time(20, 50))
+    )
+)
+def test_spans_with_the_same_start_or_stop(t1, t2, result, time_):
+    t1, t2 = TimeSpan(t1), TimeSpan(t2)
 
-
-def test_wrong_minutes():
-    with pytest.raises(ValueError):
-        TimeSpan('12:56-13:66')
-
-
-def test_start_less_than_stop():
-    with pytest.raises(ValueError):
-        TimeSpan('12:00-06:59')
-
-
-def test_intercept_equal_to_or_with_true():
-    t1, t2 = TimeSpan('12:10-13:50'), TimeSpan('13:40-14:10')
-
-    assert t1.is_intercept(t2) is t1 | t2 is True
-    assert t2.is_intercept(t1) is t2 | t1 is True
-
-
-def test_intercept_equal_to_or_with_false():
-    t1, t2 = TimeSpan('12:10-12:20'), TimeSpan('12:21-12:50')
-
-    assert t1.is_intercept(t2) is t1 | t2 is False
-    assert t2.is_intercept(t1) is t2 | t1 is False
-
-
-def test_equality_of_operands_order():
-    t1, t2 = TimeSpan('12:10-12:20'), TimeSpan('12:21-12:50')
-
-    assert t1.is_intercept(t2) is t1 | t2 is False
-    assert t2.is_intercept(t1) is t2 | t1 is False
-
-
-def test_not_intercepting_spans():
-    t1, t2 = TimeSpan('12:10-12:20'), TimeSpan('06:21-10:50')
-
-    assert t1 | t2 is False
-    assert t2 | t1 is False
-
-
-def test_equal_spans():
-    t1, t2 = TimeSpan('12:10-12:20'), TimeSpan('12:10-12:20')
-
-    assert t1 | t2 is True
-    assert t2 | t1 is True
-
-
-def test_intercepting_spans():
-    t1, t2 = TimeSpan('21:55-23:15'), TimeSpan('22:00-23:45')
-
-    assert t1 | t2 is True
-    assert t2 | t1 is True
-
-
-def test_one_span_inside_another():
-    t1, t2 = TimeSpan('10:55-23:15'), TimeSpan('12:00-19:45')
-
-    assert t1 | t2 is True
-    assert t2 | t1 is True
-
-
-def test_spans_with_the_same_start():
-    t1, t2 = TimeSpan('21:55-22:15'), TimeSpan('21:55-23:45')
-
-    assert t1 | t2 is True
-    assert t2 | t1 is True
-    assert t1.start == t2.start == time(21, 55)
-
-
-def test_spans_with_the_same_stop():
-    t1, t2 = TimeSpan('10:31-20:50'), TimeSpan('17:55-20:50')
-
-    assert t1 | t2 is True
-    assert t2 | t1 is True
-    assert t1.stop == t2.stop == time(20, 50)
+    assert t1 | t2 is result
+    assert t2 | t1 is result
+    assert t1.start == t2.start == time_
 
 
 def test_repr():
