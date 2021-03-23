79936d8dd8d119a176e59ff8e634dd4426ec1fd9
kunansy <kolobov.kirill@list.ru>
2021-03-23 13:25:30 +0300

    add_couriers() fixed
    transaction added

--- src/db_api.py
+++ src/db_api.py
@@ -65,23 +65,24 @@ class Database:
     async def add_couriers(self,
                            couriers: list) -> None:
         async with self._pool.acquire() as conn:
-            query = COMMANDS['insert']['courier']
-            couriers = ', '.join(
-                f"({courier.courier_id}, "
-                f"(SELECT t.id FROM courier_type t WHERE t.type = {courier.courier_type}),"
-                f"'{{{', '.join(courier.regions)}}}',"
-                f"'{{{', '.join(courier.working_hours)}}}'"
-                ")"
-                for courier in couriers
-            )
-            query = f"{query} {couriers};"
-            logger.debug(f"Requested to the database: \n {query}")
+            async with conn.transaction():
+                query = COMMANDS['insert']['courier']
+                couriers = ', '.join(
+                    f"({courier.courier_id}, "
+                    f"(SELECT t.id FROM courier_type t WHERE t.type = {courier.courier_type}),"
+                    f"'{{{', '.join(courier.regions)}}}',"
+                    f"'{{{', '.join(courier.working_hours)}}}'"
+                    ")"
+                    for courier in couriers
+                )
+                query = f"{query} {couriers};"
+                logger.debug(f"Requested to the database: \n {query}")
 
-            try:
-                await conn.execute(query)
-            except Exception:
-                error_logger.exception()
-                raise
+                try:
+                    await conn.execute(query)
+                except Exception:
+                    error_logger.exception()
+                    raise
 
             logger.debug(f"Couriers ({len(couriers)}) added to database")
 
